af0c45c38dff88f1d03bf3c7aa894a23dfcd58a3
wzjing <wzjing103@gmai.com>
2017-11-07 15:17:58 +0800

    Map configure

--- app/src/main/java/com/infinitytech/mapfoo/MainActivity.kt
+++ app/src/main/java/com/infinitytech/mapfoo/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.infinitytech.mapfoo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.amap.api.maps.AMap
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mapView.onCreate(savedInstanceState)
+        val map = mapView.map
+        map.mapType = AMap.MAP_TYPE_NORMAL
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
     }
 }
